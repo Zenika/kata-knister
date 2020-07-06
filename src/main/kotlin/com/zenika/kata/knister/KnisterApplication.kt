8e430798a989057d8a98d95b421390449b305caf
ffaure <frederic.faure@ext.cdiscount.com>
2020-07-06 17:12:41 +0200

    docker compose pour faire tourner mongo et knister en même temps

--- src/main/kotlin/com/zenika/kata/knister/KnisterApplication.kt
+++ src/main/kotlin/com/zenika/kata/knister/KnisterApplication.kt
@@ -1,11 +1,21 @@
 package com.zenika.kata.knister
 
+import com.zenika.kata.knister.room.MongoRoomRepository
+import com.zenika.kata.knister.room.RoomRepository
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 
 @SpringBootApplication
 open class KnisterApplication
 
 fun main(args: Array<String>) {
 	runApplication<KnisterApplication>(*args)
+}
+
+@Configuration
+class KnisterConfiguration {
+	@Bean
+	fun getRoomRepository() : RoomRepository = MongoRoomRepository()
 }
