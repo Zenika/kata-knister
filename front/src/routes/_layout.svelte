ccfcd510db15a1211980a6cbe079d5fa46a46a19
Nicolas Leport <nicolas.leport@nkcreation.com>
2020-10-08 13:30:22 +0200

    :rocket: add front starter

---
+++ front/src/routes/_layout.svelte
@@ -0,0 +1,29 @@
+<script>
+	import Nav from '@/components/Nav.svelte';
+
+	export let segment: string;
+</script>
+
+<style lang="scss" global>
+	main {
+		position: relative;
+		max-width: 56em;
+		background-color: $primary-color;
+		padding: 2em;
+		margin: 0 auto;
+		box-sizing: border-box;
+
+		div {
+			padding: 1em;
+			background: grey;
+		}
+	}
+</style>
+
+<Nav {segment}/>
+
+<main>
+<div>
+	<slot></slot>
+</div>
+</main>
