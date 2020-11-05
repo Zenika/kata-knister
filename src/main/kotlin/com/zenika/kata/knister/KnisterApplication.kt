59245d44ff8461a0a1d6006f68e74c29e34da43f
ffaure <frederic.faure@ext.cdiscount.com>
2020-11-05 11:28:01 +0100

    ajout de swagger

--- src/main/kotlin/com/zenika/kata/knister/KnisterApplication.kt
+++ src/main/kotlin/com/zenika/kata/knister/KnisterApplication.kt
@@ -7,8 +7,13 @@ import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spring.web.plugins.Docket
+import springfox.documentation.swagger2.annotations.EnableSwagger2
 
 @SpringBootApplication(exclude = arrayOf(MongoAutoConfiguration::class))
+@EnableSwagger2
 open class KnisterApplication
 
 fun main(args: Array<String>) {
@@ -19,4 +24,7 @@ fun main(args: Array<String>) {
 class KnisterConfiguration {
 	@Bean
 	fun getRoomRepository() : RoomRepository = MongoRoomRepository()
+
+	@Bean
+	fun productApi() : Docket  = Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.basePackage("com.zenika.kata.knister.room")).build()
 }
