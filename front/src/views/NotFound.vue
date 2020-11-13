fbb995e070ac3fec3c27a5f2fb6263d120287512
Nicolas Leport <nicolas.leport@nkcreation.com>
2020-11-14 00:16:45 +0100

    :art: 404 page

--- front/src/views/NotFound.vue
+++ front/src/views/NotFound.vue
@@ -1,5 +1,27 @@
 <template>
   <div>
-    not found
+    <h1>4<img src="@/assets/icon.svg" />4</h1>
+    <p>
+      Page non trouvée, <router-link to="/">Revenir à l'accueil</router-link>
+    </p>
   </div>
 </template>
+
+<style scoped>
+h1 {
+  margin: 0;
+  font-size: 40rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+img {
+  width: 0.8em;
+  height: 0.8em;
+  display: block;
+}
+
+p {
+  text-align: center;
+}
+</style>
